52dfdef32f37df1bd880adb771fe96b8c9afa9b8
Jakob <jakob.fellner@tum.de>
2022-01-13 17:10:55 +0100

    Updates

---
+++ precache-manifest.88f591d9f54562919428b356acc0cc8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd60cce47af15265702c4ca07e3a0f9",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "5b92195005a5612c0aa0",
+    "url": "/react-deploy/static/css/main.c2265363.chunk.css"
+  },
+  {
+    "revision": "9617d85a9b48e79d22f6",
+    "url": "/react-deploy/static/js/2.1cdb8da2.chunk.js"
+  },
+  {
+    "revision": "f3119454be47de5edd5bb4f41fd0e08e",
+    "url": "/react-deploy/static/js/2.1cdb8da2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d81438e2ee33dc5d3a3c",
+    "url": "/react-deploy/static/js/3.bc33ece8.chunk.js"
+  },
+  {
+    "revision": "5b92195005a5612c0aa0",
+    "url": "/react-deploy/static/js/main.a083c193.chunk.js"
+  },
+  {
+    "revision": "e0bf6e9eb9811e465853",
+    "url": "/react-deploy/static/js/runtime-main.af98d275.js"
+  },
+  {
+    "revision": "9cfe50b68736f61d061af526da6aaf14",
+    "url": "/react-deploy/static/media/bezier.9cfe50b6.cjs"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-deploy/precache-manifest.0cf4352f76566470b744615aec15f34e.js"
+  "/react-deploy/precache-manifest.88f591d9f54562919428b356acc0cc8f.js"
 );
 
 self.addEventListener('message', (event) => {
